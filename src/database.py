443744e9cf7ae835e207763cdd03fd9eca8482bf
Naren <narendas10@gmail.com>
2023-12-01 14:12:11 +0530

    Streamlit deploy

--- src/database.py
+++ src/database.py
@@ -13,5 +13,35 @@ class DatabaseConnect:
     def __init__(self):
         self.database_connect_config = DatabaseConnectConfig()
 
-    def get_data_from_database(self):
-        pass
+    def create_database(self):
+        conn = sqlite3.connect(self.database_connect_config.database_path)
+        cursor = conn.cursor()
+        create_table = """CREATE TABLE IF NOT EXISTS user_details(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            age REAL,
+            sex TEXT,
+            bmi REAL,
+            children REAL,
+            smoker TEXT,
+            region TEXT,
+            expenses REAL);"""
+        cursor.execute(create_table)
+        conn.commit()
+        return (conn, cursor)
+
+    def insert_user_data(self, name, age, sex, bmi, children, smoker, region, expenses):
+        conn, cursor = self.create_database()
+        insert_values = """INSERT INTO user_details(
+            name, age, sex, bmi, children, smoker, region, expenses) VALUES(
+            ?, ?, ?, ?, ?, ?, ?, ?)"""
+        cursor.execute(
+            insert_values, (name, age, sex, bmi, children, smoker, region, expenses)
+        )
+        conn.commit()
+
+    def display_user_database(self):
+        conn, cursor = self.create_database()
+        fetch = cursor.execute("SELECT * FROM user_details")
+        data = fetch.fetchall()
+        return (data, cursor)
--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ from src.logger import logging
 from src.exception import CustomException
 from src.pipeline.train_pipeline import TrainPipeline
 from src.pipeline.predict_pipeline import PredictPipeline, CustomData
+from src.database import DatabaseConnect
 
 
 st.set_page_config(page_title="Insurance::Home")
@@ -30,7 +31,7 @@ authenticator = stauth.Authenticate(
 
 authentication_status = authenticator.login("Login...", "main")
 
-# Login Authentication
+# LOGIN AUTHENTICATION
 if st.session_state["authentication_status"] == False:
     st.error("Username/Password is incorrect.")
 
@@ -38,30 +39,38 @@ elif st.session_state["authentication_status"] == None:
     st.warning("Please enter your username and password.")
 
 elif st.session_state["authentication_status"] == True:
-    # Logout Button for all pages
-    authenticator.logout("Logout", "main", key="unique_key")
-    st.title("*Insurance_Premium_Prediction*")
-    st.markdown(
-        "##### (Approach To Forecast Health Insurance Premiums For Individuals.)"
+    # LOGOUT BUTTON FOR ALL PAGES
+    logout = authenticator.logout("Logout", "main", key="unique_key")
+    title = st.title("*Insurance_Premium_Prediction*")
+    tag = st.markdown(
+        "##### (--Approach To Forecast Health Insurance Premiums For Individuals.)"
     )
 
+    # FILE UPLOADER
     # st.help(st.form)   # Help function.
-    st.sidebar.caption("Please select the options below for your convenience.")
-
-    # File Uploader
     st.sidebar.file_uploader("Choose the file.")
-    page = st.sidebar.selectbox(
-        "Pages", ["About", "Training Pipeline", "Prediction Pipeline"]
+    # page = st.sidebar.selectbox(
+    #     "Pages", ["About", "Training Pipeline", "Prediction Pipeline", "Show Dataframe"]
+    # )
+    page = st.sidebar.radio(
+        "Select the options below for your convenience.",
+        [
+            "About",
+            "Training Pipeline",
+            "Prediction Pipeline",
+            "Show Dataframe",
+            "Contact",
+        ],
     )
 
-    # About page
+    # ABOUT PAGE
     if page == "About":
         st.subheader("About:")
         st.markdown(
             "The purposes of this exercise to look into different features to observe their relationship, and plot a multiple linear regression based on several features of individual such as age, physical/family condition and location against their existing medical expense to be used for predicting future medical expenses of individuals that help medical insurance to make decision on charging the premium."
         )
 
-    # Training Pipeline page
+    # TRAINING PIPELINE PAGE
     if page == "Training Pipeline":
         st.markdown("***")
         training = st.button("Do you need to train the past data? Click Here!")
@@ -72,13 +81,16 @@ elif st.session_state["authentication_status"] == True:
             accuracy = str(round(tuned_score, 2) * 100)[:2]
             st.success(f"Model trained successfully with accuracy: {accuracy}%")
 
-    # Prediction Pipeline page
+    # PREDICTION PIPELINE PAGE
     if page == "Prediction Pipeline":
         st.markdown("***")
         st.info("All fields are mandatory!")
 
-        # Form Components
+        # FORM COMPONENTS
         with st.form(key="my_form", clear_on_submit=True):
+            name = st.text_input(
+                "Please enter your name.", placeholder="Your name please..."
+            )
             age = st.number_input("Please enter your age.", 0, 100)
             sex = st.selectbox(
                 "Please enter your sex.",
@@ -107,3 +119,41 @@ elif st.session_state["authentication_status"] == True:
                 st.success(
                     "Predicted Insurance Amount ₹ {:.2f}".format(float(results[0]))
                 )
+
+                # DATABASE CONNECTION
+                connect = DatabaseConnect()
+                insert_data = connect.insert_user_data(
+                    name,
+                    age,
+                    sex,
+                    bmi,
+                    children,
+                    smoker,
+                    region,
+                    round(float(results[0]), 2),
+                )
+        st.info("To view our database, kindly navigate to the 'Show Dataframe' page.")
+
+    # SHOW DATAFRAME PAGE
+    if page == "Show Dataframe":
+        st.markdown("***")
+        st.info("*** User Details ***")
+        connect = DatabaseConnect()
+        data, cursor = connect.display_user_database()
+        columns = [desc[0] for desc in cursor.description]
+        df = pd.DataFrame(data, columns=columns)
+        st.dataframe(df.tail())
+
+    if page == "Contact":
+        st.markdown("***")
+        st.subheader("*--Contact Details--*")
+        st.text("")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown(
+                "[![Narendran Mudadi|LinkedIn](https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/narendran-mudadi/)"
+            )
+        with col2:
+            st.markdown(
+                "[![Narendran Mudadi|G-Mail](https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white)](mailto:narendas10@gmail.com?subject=Insurance_Premium)"
+            )
