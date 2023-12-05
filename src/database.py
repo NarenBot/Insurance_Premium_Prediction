0e9073b7e5a9c6d19c8ebd35c10891e99e4b297b
Naren <narendas10@gmail.com>
2023-12-06 01:08:06 +0530

    mongodb connection

--- src/database.py
+++ src/database.py
@@ -2,6 +2,7 @@ import os
 import sys
 import sqlite3
 from dataclasses import dataclass
+import pymongo
 
 
 @dataclass
@@ -13,7 +14,18 @@ class DatabaseConnect:
     def __init__(self):
         self.database_connect_config = DatabaseConnectConfig()
 
-    def create_database(self):
+    def create_mongo_database(self):
+        self.username = "insurance"
+        self.password = "root_insurance"
+        self.dbname = "insurance_db"
+        self.tabname = "insurance_tab"
+        self.url = f"mongodb+srv://{self.username}:{self.password}@cluster0.plgldst.mongodb.net/?retryWrites=true&w=majority"
+        client = pymongo.MongoClient(self.url)
+        database = client[self.dbname]
+        collection = database[self.tabname]
+        return collection
+
+    def create_lite_database(self):
         conn = sqlite3.connect(self.database_connect_config.database_path)
         cursor = conn.cursor()
         create_table = """CREATE TABLE IF NOT EXISTS user_details(
@@ -31,7 +43,7 @@ class DatabaseConnect:
         return (conn, cursor)
 
     def insert_user_data(self, name, age, sex, bmi, children, smoker, region, expenses):
-        conn, cursor = self.create_database()
+        conn, cursor = self.create_lite_database()
         insert_values = """INSERT INTO user_details(
             name, age, sex, bmi, children, smoker, region, expenses) VALUES(
             ?, ?, ?, ?, ?, ?, ?, ?)"""
@@ -40,8 +52,22 @@ class DatabaseConnect:
         )
         conn.commit()
 
+        ## Insert MongoDB:
+        collection = self.create_mongo_database()
+        document = {
+            "name": name,
+            "age": age,
+            "sex": sex,
+            "bmi": bmi,
+            "children": children,
+            "smoker": smoker,
+            "region": region,
+            "expenses": expenses,
+        }
+        collection.insert_one(document=document)
+
     def display_user_database(self):
-        conn, cursor = self.create_database()
+        conn, cursor = self.create_lite_database()
         fetch = cursor.execute("SELECT * FROM user_details")
         data = fetch.fetchall()
         return (data, cursor)
